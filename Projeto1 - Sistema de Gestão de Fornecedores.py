2fd855fee441c58440b1926597232c381c22052e
João Victor Ferreira Alves <joaovictor.fasp@gmail.com>
2025-01-10 20:51:09 -0300

    Update Projeto1 - Sistema de Gestão de Fornecedores.py

--- Projeto1 - Sistema de Gestão de Fornecedores.py
+++ Projeto1 - Sistema de Gestão de Fornecedores.py
@@ -18,26 +18,36 @@ fornecedores = {
 
 #Definir uma função que irá permitir que o usuário adicione novos fornecedores à essa lista
 def adicionar_fornecedor(fornecedores):
-    id_fornecedor = input("\n Escreva o id da nova empresa: ")
+    id_fornecedor = input("\n Escreva o id do novo fornecedor: ")
 
     #Checar se o fornecedor já está na base
     if id_fornecedor in fornecedores:
-        print("\n Ops, esta empresa já está cadastrada! ")
+        print("\n Ops, este fornecedor já está cadastrado! ")
     
     #Caso não esteja cadastrada, aí sim o usuário irá digitar os dados da empresa:
     else:
+
+        #Definir quais os itens que o usuário irá digitar
         nome = input("Digite o nome da empresa: ")
         categoria = input("Digite a categoria da empresa: ")
         porte = input("Digite o porte da empresa: ")
 
         #Agora que o usuário já digitou os nomes, adicionar as informações no dicionário usando como base a chave primária
         fornecedores[id_fornecedor] = {"Nome": nome, "Categoria": categoria, "Porte": porte}
-        print(f"\nFornecedor {nome} de categoria {categoria} e porte {porte} adicionado na base!")
+        print(f"\nFornecedor {nome}, de categoria {categoria} e porte {porte} adicionado na base!")
 
-        #Imprimir a lista atual de fornecedores
-        fornecedor = fornecedores[id_fornecedor]
-        for chave, valor in fornecedor.items():
-            print(f"{chave}: {valor}")
+        
+        print("\n-------- Agora, segue lista atual de fornecedores --------")
+        #Imprimir a lista atual de fornecedores. Iterar sobre cada fornecedor e seus itens do dicionário
+        for id_fornecedor, informacoes in fornecedores.items():
+            print(f"\nID do fornecedor: ", id_fornecedor)
+
+            #Iterar sobre cada um dos itens dentro de cada fornecedor (loop aninhado para o dicionário aninhado)
+            for chave, valor in informacoes.items():
+                #imprimir cada uma das chaves e o valor
+                print(f"{chave}: {valor}")
+            
+        
 
 print("######################### Seja Bem Vindo ao mini sistema de gestão de Fornecedores ##################################")
 print("\n A lista de atual de fornecedores é a seguinte: ", fornecedores)
